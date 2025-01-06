fea5c6b5fd55b8090785a76dedb2ec0de90ef3e1
alirfanyasin <risayunita21@gmail.com>
2025-01-06 22:43:53 +0700

    fix: test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -324,8 +324,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
   {{-- Driver JS  --}}
-  <script src="https://cdn.jsdelivr.net/npm/driver.js@1.0.1/dist/driver.js.iife.js"></script>
-  </script>
+  {{-- <script src="https://cdn.jsdelivr.net/npm/driver.js@1.0.1/dist/driver.js.iife.js"></script>
+  </script> --}}
   @livewireScripts
   {{-- Livewire Alert --}}
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
